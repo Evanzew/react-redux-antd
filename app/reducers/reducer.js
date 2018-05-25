1dcb652de27035c864f0a13638b3f66eb95a2618
Evan Zou <evan.zou@perficient.com>
2018-05-25 16:15:08 +0800

    上传预览图片

--- app/reducers/reducer.js
+++ app/reducers/reducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import employees from './employees';
 import user from './login';
-import game from './game';
 
 const gameApp = combineReducers({
   user,
-  game,
   employees
 });
 
